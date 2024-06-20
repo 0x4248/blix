7974713c2c7dcf1e22338d03c20b1d05bd900d2c
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-06-20 13:44:05 +0100

    Update site
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- js/colour.js
+++ js/colour.js
@@ -7,7 +7,7 @@ colours.forEach(colour => {
 });
 
 function padZero(str, len) {
-len = len || 2;
-var zeros = new Array(len).join('0');
-return (zeros + str).slice(-len);
+    len = len || 2;
+    var zeros = new Array(len).join('0');
+    return (zeros + str).slice(-len);
 }
